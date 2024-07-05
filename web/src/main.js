cbcb061a8eebf1ffbb9bdfb81030972c0daaaa66
root <admin@example.com>
2024-07-06 01:10:40 +0800

    Fixed because of active data segment

--- web/src/main.js
+++ web/src/main.js
@@ -15,22 +15,22 @@ async function test(filename) {
   const shm0 = new WebAssembly.Memory({
     initial: 2 * Math.ceil(data.length / 2 ** 16),
   });
-  const dview = new DataView(shm0.buffer);
-  for (let i = 0; i < data.length; ++i) {
-    dview.setUint8(i, data[i]);
-  }
 
-  const msg_buf = 0;
+  const msg_buf = 2048;
   const msg_len = data.length;
   const msg_buf_len = alignToMultiplesOf(msg_len, 64);
   const result_buf = msg_buf + msg_buf_len;
-  const result_len = 224 / 8;
+  const result_len = 256 / 8;
+  const dview = new DataView(shm0.buffer);
+  for (let i = 0; i < data.length; ++i) {
+    dview.setUint8(msg_buf + i, data[i]);
+  }
 
   const hex = await WebAssembly.instantiateStreaming(fetch("digest.wasm"), {
     importobjs: { shm1: shm0 },
   })
     .then((obj) => {
-      obj.instance.exports.sha224_buffer(msg_buf, msg_len, result_buf);
+      obj.instance.exports.sha256_buffer(msg_buf, msg_len, result_buf);
       const resultDigest = new Uint8Array(shm0.buffer, result_buf, result_len);
       const hexHash = Array.from(resultDigest)
         .map((b) => b.toString(16).padStart(2, "0"))
