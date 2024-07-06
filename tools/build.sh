dff96bd041b199a5c4e2e8275c8cbb3e8305a720
root <admin@example.com>
2024-07-06 19:27:37 +0800

    Made a self-contained Dockerfile

--- tools/build.sh
+++ tools/build.sh
@@ -9,8 +9,5 @@ fi
 
 scriptDir=$(dirname $0)
 
-docker build --tag myubuntu:$tagRev $scriptDir/ubuntu
 docker build --build-arg JOBS=8 --tag mycoreutils:$tagRev $scriptDir/coreutils
-docker build --build-arg JOBS=8 --tag mywabt:$tagRev $scriptDir/wabt
-docker build --build-arg JOBS=8 --tag mybinaryen:$tagRev $scriptDir/binaryen
 docker build --tag myemcc:$tagRev $scriptDir/emscripten
