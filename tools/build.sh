8d3326d25504510cc10e9c26b8c70ce5f198f8f6
root <admin@example.com>
2024-07-06 18:03:38 +0800

    Update build script of tools

--- tools/build.sh
+++ tools/build.sh
@@ -11,8 +11,6 @@ scriptDir=$(dirname $0)
 
 docker build --tag myubuntu:$tagRev $scriptDir/ubuntu
 docker build --build-arg JOBS=8 --tag mycoreutils:$tagRev $scriptDir/coreutils
-docker build --build-arg JOBS=8 --tag mynodejs:$tagRev $scriptDir/node
 docker build --build-arg JOBS=8 --tag mywabt:$tagRev $scriptDir/wabt
-docker build --build-arg JOBS=8 --tag myllvm:$tagRev $scriptDir/llvm
 docker build --build-arg JOBS=8 --tag mybinaryen:$tagRev $scriptDir/binaryen
 docker build --tag myemcc:$tagRev $scriptDir/emscripten
